5c2d543cffabdf485b6ee0e42280aedf9cb0f08e
zhenya <zhenya9911@mail.ru>
2018-11-04 00:13:47 +0300

    Last try

--- blog/views.py
+++ blog/views.py
@@ -3,10 +3,9 @@ from django.utils import timezone
 from .models import Post
 
 
-def post_list(request, pk):
+def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
-    Post.objects.get(pk=pk)
 
 
 def post_detail(request, pk):
