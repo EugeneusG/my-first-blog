b00da8b95cfe406092c6ae35b2c00233609f52e0
zhenya <zhenya9911@mail.ru>
2018-11-03 21:09:25 +0300

    New title, posts

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from .models import Post
 
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(publishtd_date__lte=timezone.now()).order_by('publish_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
